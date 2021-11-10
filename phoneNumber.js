1b6b80f8e918a4795b079e38356b4eaf53ecf5dd
GulenYilmaz <gulenaltintasyilmaz@gmail.com>
2021-11-10 14:52:21 -0500

    done

--- phoneNumber.js
+++ phoneNumber.js
@@ -15,3 +15,20 @@
 
 // Don’t forget the space after the closing parentheses!
 
+function createPhoneNumber(numbers){
+    var format = "(xxx) xxx-xxxx";
+    
+    for(var i = 0; i < numbers.length; i++)
+    {
+      format = format.replace('x', numbers[i]);
+    }
+    
+    return format;
+  }
+  
+  
+  //brute force below
+  // function createPhoneNumber(numbers){
+  // return"(" + numbers[0] + numbers[1] + numbers[2] + ") " + numbers[3] + numbers[4] + numbers[5] + '-' + numbers[6] + numbers[7] + numbers[8] + numbers[9]
+  // }
+  
